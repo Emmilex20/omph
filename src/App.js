085c7296f89be82acbca7281e413c6c84ce61713
Emmanuel Raphael <aginaemmanuel6@gmail.com>
2025-06-26 21:23:58 +0100

    final commit 3

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+// IMPORTANT: Removed BrowserRouter import as it's now in index.js
+import { Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import HomePage from './pages/HomePage';
@@ -13,34 +14,34 @@ import ContactUsPage from './pages/ContactUsPage';
 import NewsPage from './pages/NewsPage';
 import BulletinPage from './pages/BulletinPage';
 import MassTimesPage from './pages/MassTimesPage';
-import useScrollToTop from './hooks/useScrollToTop';
+import useScrollToTop from './hooks/useScrollToTop'; // Correctly imported
 
 function App() {
-  useScrollToTop();
+  useScrollToTop(); // Hook is called here, now guaranteed to be within Router context
 
   return (
-    <Router>
-      <div className="flex flex-col min-h-screen bg-gray-50 font-sans text-dark">
-        <Navbar />
-        <main className="flex-grow">
-          <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/about" element={<AboutUsPage />} />
-            <Route path="/sacraments" element={<SacramentsPage />} />
-            <Route path="/ministries" element={<MinistriesPage />} />
-            <Route path="/events" element={<EventsPage />} />
-            <Route path="/clergy" element={<ClergyPage />} />
-            <Route path="/giving" element={<GivingPage />} />
-            <Route path="/contact" element={<ContactUsPage />} />
-            <Route path="/news" element={<NewsPage />} />
-            <Route path="/bulletin" element={<BulletinPage />} />
-            <Route path="/mass-times" element={<MassTimesPage />} />
-            {/* Add more routes for additional pages */}
-          </Routes>
-        </main>
-        <Footer />
-      </div>
-    </Router>
+    // IMPORTANT: Removed the <Router> component from here.
+    // The main <Router> should only exist once, in your index.js file.
+    <div className="flex flex-col min-h-screen bg-gray-50 font-sans text-dark">
+      <Navbar />
+      <main className="flex-grow">
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/about" element={<AboutUsPage />} />
+          <Route path="/sacraments" element={<SacramentsPage />} />
+          <Route path="/ministries" element={<MinistriesPage />} />
+          <Route path="/events" element={<EventsPage />} />
+          <Route path="/clergy" element={<ClergyPage />} />
+          <Route path="/giving" element={<GivingPage />} />
+          <Route path="/contact" element={<ContactUsPage />} />
+          <Route path="/news" element={<NewsPage />} />
+          <Route path="/bulletin" element={<BulletinPage />} />
+          <Route path="/mass-times" element={<MassTimesPage />} />
+          {/* Add more routes for additional pages */}
+        </Routes>
+      </main>
+      <Footer />
+    </div>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router } from 'react-router-dom'; // Import BrowserRouter here
 import './index.css'; // This imports your Tailwind CSS
 import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -7,7 +8,9 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Router> {/* Moved Router here to wrap the entire App */}
+      <App />
+    </Router>
   </React.StrictMode>
 );
 
